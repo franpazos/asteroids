cc9469b324ab9b54ccc027798b9118e69ca7ffe0
mq-fran <177647826+mq-fran@users.noreply.github.com>
2025-03-22 13:08:06 +0100

    Added FPS

--- main.py
+++ main.py
@@ -4,13 +4,17 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0 # delta time
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return 
+            
         screen.fill((0, 0, 0), rect=None, special_flags=0)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
