03dbcdc4e58e7371a9b92c692adee4ecf54e316b
mq-fran <177647826+mq-fran@users.noreply.github.com>
2025-03-23 02:49:49 +0100

    Adding movement for player

--- main.py
+++ main.py
@@ -4,20 +4,29 @@ from player import Player
 
 def main():
     pygame.init()
+    pygame.display.set_caption("Asteroids")
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0 # delta time
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    player.containers = (updatable, drawable)
+
+    dt = 0 # delta time
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return 
             
+        updatable.update(dt)
         screen.fill((0, 0, 0), rect=None, special_flags=0)
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
-        
+
         # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
